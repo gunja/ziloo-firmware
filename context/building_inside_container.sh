a4b54a808c4b4b262fc7fcc0de96e5ef6b644977
I F Gushchin @ Dell G3 Ubuntu 18.04 x64 <konara@yandex.ru>
2021-09-17 15:03:35 +0400

    scenario fix of auto-build

--- context/building_inside_container.sh
+++ context/building_inside_container.sh
@@ -1,5 +1,6 @@
 /repo init
 .repo/repo/repo init  --no-clone-bundle --repo-url https://gitlab.com/firefly-linux/git-repo.git -u https://gitlab.com/firefly-linux/manifests.git -b master -m rv1126_rv1109_linux_release.xml
 .repo/repo/repo sync -c
+./build.sh aio-rv1126-jd4.mk
 FORCE_UNSAFE_CONFIGURE=1 ./build.sh
 
